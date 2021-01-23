31951a1a85ac832cabb86d5f4231d37fe1573a42
optionallychained <6907442+optionallychained@users.noreply.github.com>
2021-01-23 19:04:49 +0000

    add Highlight.js syntax highlighting for docs

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,5 +1,7 @@
 const pug = require('pug');
 const marked = require('marked');
+const hljs = require('highlight.js');
+
 const fs = require('fs-extra');
 const path = require('path');
 const { exec } = require('child_process');
@@ -13,11 +15,21 @@ const staticSource = path.join(source, 'static');
 const dest = './docs';
 const staticDest = path.join(dest, 'static');
 
+// pretty filename => pagetitle mappings
 const pageNameMappings = {
     'index': 'Home'
 }
 
+// title case transformer for page titles
 const titleCase = (str) => str.replace('-', ' ').replace(/\w\S*/g, (s) => s.charAt(0).toUpperCase() + s.substr(1));
+
+// set up marked to highlight code with highlight.js
+marked.setOptions({
+    // fix a bug where the hljs class is not added to the <code> element for some reason
+    langPrefix: 'hljs ',
+    // replace return characters with <br/> in highlighted code because Pug is finnicky with newlines
+    highlight: (code) => hljs.highlightAuto(code).value.replace(/(?:\r\n|\r|\n)/g, '<br/>')
+});
 
 (async () => {
     try {
@@ -26,14 +38,17 @@ const titleCase = (str) => str.replace('-', ' ').replace(/\w\S*/g, (s) => s.char
         }
 
         // for every page, render a pug template extending the main layout file and including the compiled markdown for its 'content' block
-        for (const file of await fs.promises.readdir(pageSource)) {
+        for (const file of await fs.promises.readdir(pageSource, 'utf8')) {
             const srcPath = path.join(pageSource, file);
             const pageName = file.replace('.md', '');
             const pageNamePretty = titleCase(pageNameMappings[pageName] ?? pageName);
             const destName = file.replace('.md', '.html');
             const destPath = path.join(dest, destName);
 
+            // remove any return characters because Pug is finnicky
             const markdown = marked((await fs.promises.readFile(srcPath)).toString()).replace(/(?:\r\n|\r|\n)/g, '');
+
+            // nb: the strange layout of the template string is the only way I've found to avoid Pug complaining about extension/blocks due to indent problems
             const page = pug.render(
                 `extends layout\nblock content
                 ${markdown}`,
