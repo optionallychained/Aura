f49ec6a7a3fca52523efe223c8f69e4cfa08ca54
optionallychained <6907442+optionallychained@users.noreply.github.com>
2021-01-31 18:08:44 +0000

    include new math in barrel

--- src/engine/protogl.ts
+++ src/engine/protogl.ts
@@ -15,6 +15,9 @@ export * from './input/keys';
 export * from './math/mathUtils';
 export * from './math/vec2';
 export * from './math/vec3';
+export * from './math/vec4';
+export * from './math/mat3';
+export * from './math/mat4';
 
 export * from './screen/canvas.renderer';
 
