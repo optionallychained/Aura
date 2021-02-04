b44485ea8b83f6f53759b752c456365009c27f63
optionallychained <6907442+optionallychained@users.noreply.github.com>
2021-02-04 00:06:21 +0000

    full test suites for Angle and Random

---
+++ src/test/engine/math/angle.test.ts
@@ -0,0 +1,35 @@
+import { Angle } from '../../../engine/protogl';
+
+/**
+ * Tests for class Angle
+ */
+describe('Angle', () => {
+
+    /**
+     * Tests for Angle.toRadians()
+     */
+    describe('toRadians', () => {
+        it('should convert degrees to radians correctly', () => {
+            const deg = 36;
+            const rad = Math.PI / 180;
+
+            const result = Angle.toRadians(deg);
+
+            expect(result).toBe(deg * rad);
+        });
+    });
+
+    /**
+     * Tests for Angle.toDegrees()
+     */
+    describe('toDegrees', () => {
+        it('should convert radians to degrees correctly', () => {
+            const rad = 2.4;
+            const deg = 180 / Math.PI;
+
+            const result = Angle.toDegrees(rad);
+
+            expect(result).toBe(rad * deg);
+        });
+    });
+});
---
+++ src/test/engine/math/random.test.ts
@@ -0,0 +1,25 @@
+import { Random } from '../../../engine/protogl';
+
+/**
+ * Tests for class Random
+ */
+describe('Random', () => {
+
+    /**
+     * Tests for Random.between()
+     */
+    describe('between', () => {
+        it('should clamp random values correctly', (done) => {
+            const min = 1, max = 2, iterations = 1000;
+
+            for (let i = 0; i < iterations; i++) {
+                const random = Random.between(min, max);
+
+                expect(random).toBeGreaterThanOrEqual(min);
+                expect(random).toBeLessThan(max);
+            }
+
+            done();
+        });
+    });
+});
