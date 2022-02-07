30841a7b915a6394a1203a87527d379233220d63
optionallychained <6907442+optionallychained@users.noreply.github.com>
2022-02-07 14:29:49 +0000

    wireframe octahedron geometry

--- src/engine/geometry/3d/wireframe/index.ts
+++ src/engine/geometry/3d/wireframe/index.ts
@@ -1,4 +1,5 @@
 export * from './box.wireframe.geometry.3d';
+export * from './octahedron.wireframe.geometry.3d';
 export * from './prism.wireframe.geometry.3d';
 export * from './pyramid.wireframe.geometry.3d';
 export * from './tetrahedron.wireframe.geometry.3d';
