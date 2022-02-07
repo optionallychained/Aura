9ec02cc3c442986498c2417201de175f5352fce6
optionallychained <6907442+optionallychained@users.noreply.github.com>
2022-02-07 17:15:24 +0000

    wireframe hexagonal pyramid geoemtry

--- src/engine/geometry/3d/wireframe/index.ts
+++ src/engine/geometry/3d/wireframe/index.ts
@@ -2,5 +2,6 @@ export * from './box.wireframe.geometry.3d';
 export * from './octahedron.wireframe.geometry.3d';
 export * from './prismHexagonal.wireframe.geometry';
 export * from './prismTriangular.wireframe.geometry.3d';
+export * from './pyramidHexagonal.wireframe.geometry.3d';
 export * from './pyramidSquare.wireframe.geometry.3d';
 export * from './tetrahedron.wireframe.geometry.3d';
--- src/engine/geometry/3d/wireframe/pyramidHexagonal.wireframe.geometry.3d.ts
+++ src/engine/geometry/3d/wireframe/pyramidHexagonal.wireframe.geometry.3d.ts
@@ -2,13 +2,13 @@ import { GLShape } from '../../glShape.enum';
 import { Geometry } from '../../geometry';
 
 /**
- * Built-in 3D wireframe prism Geometry, setting out the information required to render wireframe hexagonal prisms
+ * Built-in 3D wireframe pyramid Geometry, setting out the information required to render wireframe hexagon-based pyramids
  */
 
 const h = Math.sqrt(3) / 4;
 
-export const PRISM_HEXAGONAL = new Geometry({
-    name: 'prism_hexagonal_wireframe',
+export const PYRAMID_HEXAGONAL = new Geometry({
+    name: 'pyramid_hexagonal_wireframe',
     vertices: Float32Array.from([
         // base
         -0.25, -0.5, h,
@@ -20,36 +20,29 @@ export const PRISM_HEXAGONAL = new Geometry({
 
         // front face
         -0.25, -0.5, h,
-        -0.25, 0.5, h,
-        0.25, 0.5, h,
-        0.25, -0.5, h,
+        0, 0.5, 0,
 
         // right front face
-        0.25, 0.5, h,
-        0.5, 0.5, 0,
-        0.5, -0.5, 0,
+        0.25, -0.5, h,
+        0, 0.5, 0,
 
         // right back face
-        0.5, 0.5, 0,
-        0.25, 0.5, -h,
-        0.25, -0.5, -h,
+        0.5, -0.5, 0,
+        0, 0.5, 0,
 
         // back face
-        0.25, 0.5, -h,
-        -0.25, 0.5, -h,
-        -0.25, -0.5, -h,
+        0.25, -0.5, -h,
+        0, 0.5, 0,
 
         // left back face
-        -0.25, 0.5, -h,
-        -0.5, 0.5, 0,
-        -0.5, -0.5, 0,
+        -0.25, -0.5, -h,
+        0, 0.5, 0,
 
         // left front face
-        -0.5, 0.5, 0,
-        -0.25, 0.5, h
+        -0.5, -0.5, 0
     ]),
     vertexSize: 3,
-    vertexCount: 24,
+    vertexCount: 17,
     glShape: GLShape.LINE_LOOP,
     textureCoordinates: Float32Array.from([
 
