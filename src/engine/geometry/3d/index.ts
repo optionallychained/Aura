a2c0fbff04d5b8735b4861ac01beaee2d6c8b52b
optionallychained <6907442+optionallychained@users.noreply.github.com>
2022-02-07 12:54:49 +0000

    prism geometry

--- src/engine/geometry/3d/index.ts
+++ src/engine/geometry/3d/index.ts
@@ -1,6 +1,7 @@
 export * from './box.geometry.3d';
 export * from './line.geometry.3d';
 export * from './point.geometry.3d';
+export * from './prism.geometry.3d';
 export * from './pyramid.geometry.3d';
 export * from './tetrahedron.geometry.3d';
 
