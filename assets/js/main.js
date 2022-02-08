5b6e3860ee117ae0f11b23c1f4567c9e0c68eee8
optionallychained <6907442+optionallychained@users.noreply.github.com>
2022-02-08 16:33:13 +0000

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";document.querySelectorAll("li.dropdown").forEach((function(o){window.location.pathname.includes(o.id)&&o.classList.add("open")}))})();
+(()=>{"use strict";console.log("docs")})();
